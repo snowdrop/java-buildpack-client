c75156a99951582d1011ebfe874089a317718576
Ioannis Canellos <iocanel@gmail.com>
2021-12-09 16:26:58 +0200

    feat: add samples folder

---
+++ samples/hello-spring/pack.java
@@ -0,0 +1,22 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+// //DEPS <dependency1> <dependency2>
+
+//DEPS dev.snowdrop:buildpack-client:0.0.2-SNAPSHOT
+import static java.lang.System.*;
+
+import java.io.File;
+import dev.snowdrop.buildpack.*;
+
+public class pack {
+
+    public static void main(String... args) {
+      try {
+      BuildpackBuilder.get()
+        .withContent(new File("."))
+        .withFinalImage("snowdrop/hello-spring:latest")
+        .build(new LogRelay());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+}
---
+++ samples/hello-spring/src/main/java/dev/snowdrop/hellospring/DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.snowdrop.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
