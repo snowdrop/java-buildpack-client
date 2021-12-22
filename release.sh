d58f2ffa482d8467495d9deeff6cbe6dc35d32f1
Ioannis Canellos <iocanel@gmail.com>
2021-12-22 09:16:24 +0200

    feat: release scripts now automatically updates readme and samples to refer to release version

--- release.sh
+++ release.sh
@@ -15,3 +15,15 @@ if [ -z $release_version ]; then
 fi
 
 maven::release $*
+
+#
+# Set release version in the samples & readme
+#
+find ./samples/ -iwholename "*/pack.java" | while read f; do
+    sed -i "s/\/\/DEPS dev.snowdrop:buildpack-client:.*/\/\/DEPS dev.snowdrop:buildpack-client:${release_version}/g" $f
+    git add $f
+done
+sed -i "s/\/\/DEPS dev.snowdrop:buildpack-client:.*/\/\/DEPS dev.snowdrop:buildpack-client:${release_version}/g" README.md
+sed -i "s/<version>.*<\/version>/<version>${release_version}<\/version>/g" README.md
+git add README.md
+git commit -m "chore: set release version in samples and README.md"
